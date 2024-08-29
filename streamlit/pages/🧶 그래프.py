4fd92d22daea01ad245adbb7220dca3fa80c1dda
noweahc <vllkey@naver.com>
2024-08-29 21:26:33 +0900

    0829

--- streamlit/pages/🧶 그래프.py
+++ streamlit/pages/🧶 그래프.py
@@ -1,86 +1,54 @@
 import streamlit as st
 import pandas as pd
-import os
 import networkx as nx
-import plotly.graph_objs as go
+from streamlit_agraph import agraph, Node, Edge, Config
 from utils.graph_utils import create_graph
 
 # 데이터 로드
 df = pd.read_csv('finaldata.csv')
 df['timestamp'] = pd.to_datetime(df['timestamp'])
 
-def plot_graph_plotly(G, pos, node_data):
-    edge_traces = []
+# 8월 7일 이후의 타임스탬프 필터링
+start_date = pd.to_datetime("2023-08-08")
+filtered_df = df[df['timestamp'] > start_date]
+
+def create_agraph(G, pos):
+    nodes = []
+    edges = []
+
+    for node in G.nodes(data=True):
+        node_name = node[0][1:]  # 첫 글자를 제외한 이름으로 설정
+        nodes.append(Node(id=node[0], label=node_name, size=10, color="lightblue"))
+
     for edge in G.edges(data=True):
-        x0, y0 = pos[edge[0]]
-        x1, y1 = pos[edge[1]]
-        
-        edge_trace = go.Scatter(
-            x=[x0, x1, None], y=[y0, y1, None],
-            line=dict(width=edge[2]['weight'], color='#888'),  # 엣지의 굵기를 개별적으로 설정
-            hoverinfo='none',
-            mode='lines'
-        )
-        edge_traces.append(edge_trace)
+        edges.append(Edge(source=edge[0], target=edge[1], type="CURVED"))
 
-    node_x = []
-    node_y = []
-    node_color = []
-    node_text = []
-
-    for node in G.nodes():
-        x, y = pos[node]
-        node_x.append(x)
-        node_y.append(y)
-        node_color.append('lightblue')  # 노드 색상 설정
-        node_name = node[1:]  # 첫 글자를 제외한 이름으로 설정
-        node_text.append(node_name)
-
-    node_trace = go.Scatter(
-        x=node_x, y=node_y,
-        mode='markers+text',
-        hoverinfo='text',
-        marker=dict(
-            showscale=False,
-            color=node_color,
-            size=20,  # 노드 크기 고정
-            line=dict(width=2, color='DarkSlateGrey')
-        ),
-        text=node_text,
-        textposition="middle center"  # 텍스트 위치를 노드 중앙에 설정
+    config = Config(
+        width=1000,
+        height=800,
+        directed=False,
+        nodeHighlightBehavior=True,
+        highlightColor="#F7A7A6",  # 강조할 때 노드의 색상
+        collapsible=True,
+        node={'labelProperty': 'label'},
+        link={'labelProperty': 'label', 'renderLabel': True}
     )
 
-    fig = go.Figure(data=edge_traces + [node_trace],
-                    layout=go.Layout(
-                        title='워크샵 인물 관계 그래프',
-                        titlefont_size=16,
-                        showlegend=False,
-                        hovermode='closest',
-                        margin=dict(b=0,l=0,r=0,t=0),
-                        annotations=[dict(
-                            text="",
-                            showarrow=False,
-                            xref="paper", yref="paper"
-                        )],
-                        xaxis=dict(showgrid=False, zeroline=False),
-                        yaxis=dict(showgrid=False, zeroline=False))
-                    )
-    return fig
+    return nodes, edges, config
 
 def show():
     st.title("9기의 워크샵 인물 관계 그래프 시각화")
 
-    time_point = st.slider(
+    time_point = st.select_slider(
         '시간 선택',
-        min_value=df['timestamp'].min().to_pydatetime(),
-        max_value=df['timestamp'].max().to_pydatetime(),
-        value=df['timestamp'].min().to_pydatetime(),
-        format="YYYY-MM-DD HH:mm:ss"
+        options=filtered_df['timestamp'].unique(),
+        value=filtered_df['timestamp'].min()
     )
 
     G, pos, sub_df = create_graph(time_point, df)
-    fig = plot_graph_plotly(G, pos, sub_df)
+    nodes, edges, config = create_agraph(G, pos)
 
-    st.plotly_chart(fig)
+    agraph(nodes=nodes, edges=edges, config=config)
+
 if __name__ == "__main__":
     show()
